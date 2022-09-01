ce9b8efa77fd638cc4936df3a3594471ab6d2d98
Flutter Developer <fdeveloper786@gmail.com>
2022-09-01 23:31:18 +0530

    Initial Commit

---
+++ Shared/SplashSliderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashSliderAppApp.swift
+//  Shared
+//
+//  Created by Mohammad Jawed Ahmad Ansari on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SplashSliderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
